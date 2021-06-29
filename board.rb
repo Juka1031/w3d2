a08abe16606ebd3a0bba8939905471e6d1218cb3
Sydney <sydneyparsonsyb@gmail.com>
2021-06-29 15:00:40 -0400

    4th

--- board.rb
+++ board.rb
@@ -24,16 +24,35 @@ class Board
         while @grid.count(nil) != 0 #while the grid contains nil
 
             pair.each do |card| #ABCDEFGH
-                x = rand(0...@grid.length) #x= 0
-                y = rand(0...@grid.length)  #y=0
-                if @grid[x][y] == nil #if spot is empty
-                    @grid[x][y] = Card.new(card) #intialize new card instance
+                # x = rand(0...@grid.length) #x= 0
+                # y = rand(0...@grid.length)  #y=0
+                # if @grid[x][y] == nil #if spot is empty
+                #     @grid[x][y] = Card.new(card) #intialize new card instance
+                # else
+                placed = false
+                while placed != true
+                    x = rand(0...@grid.length) #x= 0
+                    y = rand(0...@grid.length)
+                    if @grid[x][y] == nil
+                        placed = true
+                        @grid[x][y] = Card.new(card) #intialize new card instance
+                    end
                 end
             end
         end
     end
 
     def render
+        count = 0
+        @grid.length.times do
+            print "#{count} "
+            count += 1
+        end
+
+        @grid.each_with_index do |sub_array, idx|
+            puts "#{idx} " + sub_array.join(" ")
+        end
+
     end
 
     def won?
