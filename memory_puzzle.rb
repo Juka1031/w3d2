fd13e9c94b8099b7fbc3bd680bdedc31d596955e
Sydney <sydneyparsonsyb@gmail.com>
2021-06-29 13:50:26 -0400

    second

--- memory_puzzle.rb
+++ memory_puzzle.rb
@@ -1,1 +1,16 @@
-#hello
+
+class Card
+
+    def initialize(char)
+        @state = false
+        @char = char
+    end
+
+    def turn_over
+        @state = !@state
+    end
+
+    def to_s
+
+
+end
