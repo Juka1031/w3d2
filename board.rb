ddea8d2ddcdf095a74c2af7b125a78885ccd72cc
Sydney <sydneyparsonsyb@gmail.com>
2021-06-29 14:22:41 -0400

    3rd

--- board.rb
+++ board.rb
@@ -6,13 +6,29 @@ class Board
         if !(num.is_even?)
             raise "enter even number"
         end
-        @grid = Array.new(num) {Array.new(num)}
+        @grid = Array.new(num) { Array.new(num, nil) }
     end
 
     def populate
         alpha = ("A".."Z").to_a
         #lets populate this grid with num**2 / 2 pairs
-        
+        pairs = @grid.length ** 2 / 2
+        pair = []
+        while pair.length != pairs
+            picked = alpha.sample
+            if !pair.include?(picked)
+                pair << picked
+            end
+        end
+
+        while @grid.count(nil) != 0
+            x = rand(0...@grid.length)
+            y = rand(0...@grid.length)
+
+            
+
+
+
     end
 
     def render
